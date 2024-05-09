634ddedf11bf39ad9b85edd71b89cabdc67d888d
architbatham <71764919+architbatham@users.noreply.github.com>
2024-05-09 12:52:17 +0530

    program2 archit

--- program2.js
+++ program2.js
@@ -1,7 +1,33 @@
-const decodeTheRing = function (s, p) {
+function decoderRing(s, p) {
+  let sIndex = 0;
+  let pIndex = 0;
 
-    // write your code here
+  while (sIndex < s.length && pIndex < p.length) {
+      if (p[pIndex] === '*') {
+          while (pIndex < p.length && p[pIndex] === '*') {
+              pIndex++;
+          }
+          if (pIndex === p.length) {
+              return true;
+          }
+          while (sIndex < s.length && s[sIndex] !== p[pIndex]) {
+              sIndex++;
+          }
+          if (sIndex === s.length) {
+              return false;
+          }
+      } else if (p[pIndex] === '?' || p[pIndex] === s[sIndex]) {
+          pIndex++;
+          sIndex++;
+      } else {
+          return false;
+      }
+  }
 
-  };
-  
-  module.exports = decodeTheRing;
+  return sIndex === s.length && pIndex === p.length;
+}
+
+// Test cases
+console.log(decoderRing("aa", "a"));   // Output: false
+console.log(decoderRing("aa", "*"));   // Output: true
+console.log(decoderRing("cb", "?a")); // Output: false
